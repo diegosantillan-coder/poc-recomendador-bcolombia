4e3d88e353b610482e8169d880ccb14037a62d2a
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-09-04 12:50:35 -0500

    feat: agregar colita chat usuario

--- src/app/ui/atoms/a-card-chat/a-card-chat.component.ts
+++ src/app/ui/atoms/a-card-chat/a-card-chat.component.ts
@@ -1,12 +1,13 @@
 import { NgClass } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
 	selector: 'a-card-chat',
 	standalone: true,
 	imports: [NgClass],
 	templateUrl: './a-card-chat.component.html',
-	styleUrl: './a-card-chat.component.scss'
+	styleUrl: './a-card-chat.component.scss',
+  encapsulation: ViewEncapsulation.None, // O encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ACardChatComponent {
 	@Input() chats: { text: string; isUser: boolean }[] = [];
