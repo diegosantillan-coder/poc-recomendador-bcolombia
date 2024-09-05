96fe42bc05676a3fd527bbeedb195b97dfd486ce
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-09-04 19:20:33 -0500

    feat: agregar contenedor home

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { OrganismsModule } from '@ui/organisms/organisms.module';
+import { AtomsModule } from '../../ui/atoms/atoms.module';
 
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [OrganismsModule],
+	imports: [OrganismsModule, AtomsModule],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss'
 })
