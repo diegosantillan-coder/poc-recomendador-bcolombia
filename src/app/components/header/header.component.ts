ab7c0923cb3adea4a0809cd68eb307deed472dc1
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-09-05 04:15:54 -0500

    fix: tamaño steps

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalService } from '@core/services/modal/modal.service';
+import { OnboardingService } from '@core/services/onboarding/onboarding.service';
 import { OrganismsModule } from '@ui/organisms/organisms.module';
 import { AtomsModule } from '../../ui/atoms/atoms.module';
 
@@ -12,10 +14,14 @@ import { AtomsModule } from '../../ui/atoms/atoms.module';
 })
 export class HeaderComponent {
 	private readonly router = inject(Router);
+	private readonly onboardingService = inject(OnboardingService);
+	private readonly modalService = inject(ModalService);
 
 	exit() {
 		this.router.navigate(['/login']);
 		localStorage.removeItem('user');
 		localStorage.removeItem('onboarding');
+		this.onboardingService.setOnboarding(false);
+		this.modalService.setModalStatus(false);
 	}
 }
