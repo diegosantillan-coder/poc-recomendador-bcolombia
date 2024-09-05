b12f7d14b4a5988b07f29412864e413c8ab4d4b7
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-09-05 00:35:46 -0500

    fix: remover elementos no usados

--- src/app/ui/templates/templates.module.ts
+++ src/app/ui/templates/templates.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { THomeBankiaComponent } from './t-home-bankia/t-home-bankia.component';
-import { THomeComponent } from './t-home/t-home.component';
 import { TLoginBankiaComponent } from './t-login-bankia/t-login-bankia.component';
 import { TOnboardingComponent } from './t-onboarding/t-onboarding.component';
 
@@ -9,16 +8,10 @@ import { TOnboardingComponent } from './t-onboarding/t-onboarding.component';
 	declarations: [],
 	imports: [
 		CommonModule,
-		THomeComponent,
 		TLoginBankiaComponent,
 		THomeBankiaComponent,
 		TOnboardingComponent
 	],
-	exports: [
-		THomeComponent,
-		TLoginBankiaComponent,
-		THomeBankiaComponent,
-		TOnboardingComponent
-	]
+	exports: [TLoginBankiaComponent, THomeBankiaComponent, TOnboardingComponent]
 })
 export class TemplatesModule {}
