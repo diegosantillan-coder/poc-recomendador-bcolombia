3ff053019a9fe464fddf4a03c639a570ccccc169
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-10-28 11:24:18 -0500

    feat: ui banner kitchen olimpica

--- src/app/components/olimpica/home-olimpica/home-olimpica.component.ts
+++ src/app/components/olimpica/home-olimpica/home-olimpica.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ACardLocationComponent } from "../../../ui/atoms/a-card-location/a-card-location.component";
+import { ALogoComponent } from "../../../ui/atoms/a-logo/a-logo.component";
 import { MNavbarOlimpicaComponent } from "../../../ui/molecules/m-navbar-olimpica/m-navbar-olimpica.component";
 import { HeaderOlimpicaComponent } from "../header-olimpica/header-olimpica.component";
 
 @Component({
   selector: 'app-home-olimpica',
   standalone: true,
-  imports: [HeaderOlimpicaComponent, MNavbarOlimpicaComponent],
+  imports: [HeaderOlimpicaComponent, MNavbarOlimpicaComponent, ACardLocationComponent, ALogoComponent],
   templateUrl: './home-olimpica.component.html',
   styleUrl: './home-olimpica.component.scss'
 })
